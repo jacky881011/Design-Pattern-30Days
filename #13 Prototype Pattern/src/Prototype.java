c68b7f4f9434ec4e5252887542389d98abbbd882
Ming Yun <jacky.hsu.tw1@gmail.com>
2021-08-05 13:46:36 +0800

    0805 Prototype Pattern

---
+++ #13 Prototype Pattern/src/Prototype.java
@@ -0,0 +1,11 @@
+
+// Interface
+
+public abstract class Prototype implements Cloneable{
+    public Prototype clone() throws CloneNotSupportedException
+    {
+        return (Prototype) super.clone();
+    }
+
+
+}
---
+++ #13 Prototype Pattern/src/Sensor.java
@@ -0,0 +1,74 @@
+public class Sensor extends Prototype {
+    private int ID;
+    private String name;
+    private String nickname;
+
+    private String version;
+    private String wifiID;
+    private String wifiPass;
+
+    private String data;
+
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getNickname(){
+        return nickname;
+    }
+
+    public void setNickname(String nickname){
+        this.nickname = nickname;
+    }
+
+    public String getVersion(){
+        return version;
+    }
+
+    public void setVersion(String version){
+        this.version = version;
+    }
+
+    public String getWifiID(){
+        return wifiID;
+    }
+
+    public void setWifiID(String wifiID){
+        this.wifiID = wifiID;
+    }
+
+    public String getWifiPass(){
+        return wifiPass;
+    }
+
+    public void setWifiPass(String wifiPass){
+        this.wifiPass = wifiPass;
+    }
+
+    // 務必要加上這行去覆寫
+    @Override
+    public Prototype clone() throws CloneNotSupportedException{
+        return (Sensor) super.clone();
+    }
+
+}
